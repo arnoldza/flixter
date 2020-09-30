054eb53e3b3140addbd70fbad699022df1dd46f8
Zach Arnold <arnoldza@msu.edu>
2020-09-29 23:17:22 -0400

    Initial Commit

---
+++ flixter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixter
+//
+//  Created by Zach Arnold on 9/29/20.
+//  Copyright © 2020 arnoldza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
