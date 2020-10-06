76fdb8416d35c3516a5a753cad824eb0a0f210ca
Zach Arnold <arnoldza@msu.edu>
2020-10-05 23:13:03 -0400

    Final commit for Flixter

---
+++ flixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Zach Arnold on 10/5/20.
+//  Copyright © 2020 arnoldza. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
